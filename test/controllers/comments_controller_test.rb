736b256239cff8d8d9df9b4a410ec33083755cb9
Igor Inkovskiy <Inkovskiy@gmail.com>
2018-03-03 23:48:59 +0300

    test(Comment) add tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = articles(:one)
+    @comment = comments(:one)
+  end
+
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post article_comments_url(@article), params:
+        {
+          comment:
+            {
+              commenter: @comment.commenter,
+              body: @comment.body,
+              article_id: @article.id
+            }
+        }
+    end
+
+    assert_response :redirect
+  end
+
+  test "should destroy comment" do
+    assert_difference('Comment.count', -1) do
+      delete article_comment_url(@article, @comment)
+    end
+
+    assert_response :redirect
+  end
 end
