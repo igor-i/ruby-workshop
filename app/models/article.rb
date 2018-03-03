d3226d3077ae1c195a85228059ead225f3b90441
Igor Inkovskiy <Inkovskiy@gmail.com>
2018-03-03 13:09:08 +0300

    steps 1-7

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+  validates :author_name, presence: true
+  validates :state, inclusion: { in: %w(draft published),
+    # message: t('state_validates', value: value) }
+    message: "%{value} is not a valid state" }
+end
